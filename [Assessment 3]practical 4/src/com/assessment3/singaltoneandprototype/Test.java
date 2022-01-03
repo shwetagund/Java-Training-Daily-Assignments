5c01b618d741be96571435139faf9eff1bf9dac6
shwetagund <shwetagund5566@gmail.com>
2022-01-03 10:47:24 +0530

    Assignments

---
+++ [Assessment 3]practical 4/src/com/assessment3/singaltoneandprototype/Test.java
@@ -0,0 +1,18 @@
+package com.assessment3.singaltoneandprototype;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("Sample.xml");
+		Student st = (Student)applicationContext.getBean("stud1"); 
+		
+		System.out.println(st.hashCode()); //this is first hashcode
+		
+		Student st2 = (Student)applicationContext.getBean("stud2");
+		System.out.println(st2.hashCode());//this is diff hashcode
+
+	}
+
+}
